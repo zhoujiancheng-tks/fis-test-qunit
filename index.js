e88b7e0fd9c302da9086ff3a170914062c716168
jasonzhoujc <jason.zhoujc@gmail.com>
2013-12-23 13:23:08 +0800

    fis-test-qunit

---
+++ index.js
@@ -0,0 +1,14 @@
+/*
+ * 
+ * http://fis.baidu.com/
+ */
+
+'use strict';
+
+var JSHINT = require("phantomjs");
+
+module.exports = function(content, file, conf){
+    console.log(content);
+    console.log(file);
+    console.log('qunit');
+};
